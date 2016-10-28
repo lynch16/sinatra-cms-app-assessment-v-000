1f3c695f8eb281c06b1189ed66a7b5fced889e8a
Will Lynch <will.lynch91@gmail.com>
2016-10-28 16:11:56 +0000

    created entities CRUD

---
+++ app/controllers/entity_controller.rb
@@ -0,0 +1,57 @@
+class EntityController < ApplicationController
+
+    get '/entities' do
+        if !!session[:id]
+            @entities = Entity.all
+            erb :'/entities/index'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/entities/new' do
+        if !!session[:id]
+            erb :'/entities/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/entities' do
+        @user = User.find(session[:id])
+        @entity = Entity.new(params[:entity])
+        if !@entity.save
+            flash[:message]="Error saving entity. Please check entries."
+            erb :'/entities/new'
+        else
+            @entity.save
+            redirect '/entities'
+        end
+    end
+
+    get '/entities/:id' do
+        if !!session[:id]
+            @entity = Entity.find(params[:id])
+            erb :'/entities/show'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/entities/:id/edit' do
+        if !!session[:id]
+            @user = User.find(session[:id])
+            @entity = Entity.find(params[:id])
+            #update entity.last_user once column added
+            erb :'/entities/edit'
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/entities/:id' do
+    end
+
+    delete '/entities/:id/delete' do
+    end
+end
